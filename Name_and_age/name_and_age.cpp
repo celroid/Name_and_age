45fe636d6239b7053b5f6ae00e97ba8ef6d1e30c
Hyu <liliumhyu@gmail.com>
2017-10-23 15:58:23 +0300

    type double for age to allow half years, dispaly the age in months

--- Name_and_age/name_and_age.cpp
+++ Name_and_age/name_and_age.cpp
@@ -1,16 +1,16 @@
 //this program reads the name and age from the keyboard and outputs them on the console
 
-//v1.0
+//v2.0
 
 #include "../../std_lib_facilities.h"
 
 int main() {
 	cout << "Please enter your first name and age \n";
 	string first_name; // string variable
-	int age;
+	double age;
 	cin >> first_name;
 	cin >> age;
-	cout << "Hello, " << first_name << " (age " << age << ")\n";
+	cout << "Hello, " << first_name << " (age in months " << age *12 << ")\n";
 
 	keep_window_open();
 	return 0;
